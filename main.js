47b5afe3b3b5abbaf25cd4e74341b71594f83ea5
salrey <tekdehn@gmail.com>
2021-10-09 20:19:52 -0400

    Update reset, errors, helper function and DOM

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
 
+    //reset/remove old things
+    document.querySelector(".display").classList.remove("error")
+    document.querySelectorAll(".display section").forEach((section) => section.classList.add("hidden"))
+    document.querySelector(".display p").classList.remove("hidden")
+
     const location = event.target.location.value;
     
     //find errors
@@ -11,27 +16,36 @@ document.querySelector("form").addEventListener("submit", (event) => {
         document.querySelector(".display").classList.add("error")
     } else {
         //reset/remove old things
+        document.querySelector(".history p").classList.add("hidden")
         document.querySelector(".display p").classList.add("hidden")
         
         fetch(`http://wttr.in/${location}?format=j1`)
         .then((response) => response.json())
         .then((weather) => {            
             //check weather
-            console.log(weather.nearest_area[0].areaName[0].value)
+            // console.log(weather.nearest_area[0].areaName[0].value)
+
+            //create section for the main with id's specific to location
+            const section = document.createElement("section")
+            section.id = location.toLowerCase().split(" ").join("")
+            document.querySelector(".display").append(section)
 
             //create 4 articles in the main section (main-info, today, tomorrow, day-after-tomorrow)
 
             const mainInfo = document.createElement("article")
             mainInfo.id = "main-info"
+
+            const daysInfo = document.createElement("div")
             const today = document.createElement("article")
             today.id = "today"
             const tomorrow = document.createElement("article")
             tomorrow.id = "tomorrow"
             const dayAfterTomorrow = document.createElement("article")
             dayAfterTomorrow.id = "day-after-tomorrow"
+            daysInfo.append(today, tomorrow, dayAfterTomorrow)
 
-            //append all of the articles to .display section 
-            document.querySelector(".display").append(mainInfo, today, tomorrow, dayAfterTomorrow)
+            //append all of the articles to specific section 
+            section.append(mainInfo, daysInfo)
 
             //Add stuff to main-info
             const mainHeading = document.createElement("h2");
@@ -43,7 +57,7 @@ document.querySelector("form").addEventListener("submit", (event) => {
             const country = document.createElement("p");
             country.innerHTML = `<span>Country:</span> ${capitalize(weather.nearest_area[0].country[0].value)}`
             const currently = document.createElement("p");
-            currently.innerHTML = `<span>Currently:</span> Feels like ${capitalize(weather.current_condition[0].FeelsLikeF)}°F`
+            currently.innerHTML = `<span>Currently:</span> Feels like ${weather.current_condition[0].FeelsLikeF}°F`
             //append it main-info 
             mainInfo.append(mainHeading, area, region, country, currently);
 
@@ -51,36 +65,36 @@ document.querySelector("form").addEventListener("submit", (event) => {
             const todayHeading = document.createElement("h3")
             todayHeading.textContent = "Today"
             const todayAvg = document.createElement("p")
-            todayAvg.innerHTML = `<span>Average:</span> ${capitalize(weather.weather[0].avgtempF)}°F`
+            todayAvg.innerHTML = `<span>Average:</span> ${weather.weather[0].avgtempF}°F`
             const todayMax = document.createElement("p")
-            todayMax.innerHTML = `<span>Highest:</span> ${capitalize(weather.weather[0].maxtempF)}°F`
+            todayMax.innerHTML = `<span>Highest:</span> ${weather.weather[0].maxtempF}°F`
             const todayMin = document.createElement("p")
-            todayMin.innerHTML = `<span>Lowest:</span> ${capitalize(weather.weather[0].mintempF)}°F`
+            todayMin.innerHTML = `<span>Lowest:</span> ${weather.weather[0].mintempF}°F`
             today.append(todayHeading,todayAvg,todayMax, todayMin)
             
             //Add stuff to tomorrow
             const tomorrowHeading = document.createElement("h3")
             tomorrowHeading.textContent = "Tomorrow"
             const tomorrowAvg = document.createElement("p")
-            tomorrowAvg.innerHTML = `<span>Average:</span> ${capitalize(weather.weather[1].avgtempF)}°F`
+            tomorrowAvg.innerHTML = `<span>Average:</span> ${weather.weather[1].avgtempF}°F`
             const tomorrowMax = document.createElement("p")
-            tomorrowMax.innerHTML = `<span>Highest:</span> ${capitalize(weather.weather[1].maxtempF)}°F`
+            tomorrowMax.innerHTML = `<span>Highest:</span> ${weather.weather[1].maxtempF}°F`
             const tomorrowMin = document.createElement("p")
-            tomorrowMin.innerHTML = `<span>Lowest:</span> ${capitalize(weather.weather[1].mintempF)}°F`
+            tomorrowMin.innerHTML = `<span>Lowest:</span> ${weather.weather[1].mintempF}°F`
             tomorrow.append(tomorrowHeading,tomorrowAvg,tomorrowMax, tomorrowMin)
 
             //Add stuff to day-after-tomorrow 
             const dayAfterHeading = document.createElement("h3")
             dayAfterHeading.textContent = "Day After Tomorrow"
             const dayAfterAvg = document.createElement("p")
-            dayAfterAvg.innerHTML = `<span>Average:</span> ${capitalize(weather.weather[2].avgtempF)}°F`
+            dayAfterAvg.innerHTML = `<span>Average:</span> ${weather.weather[2].avgtempF}°F`
             const dayAfterMax = document.createElement("p")
-            dayAfterMax.innerHTML = `<span>Highest:</span> ${capitalize(weather.weather[2].maxtempF)}°F`
+            dayAfterMax.innerHTML = `<span>Highest:</span> ${weather.weather[2].maxtempF}°F`
             const dayAfterMin = document.createElement("p")
-            dayAfterMin.innerHTML = `<span>Lowest:</span> ${capitalize(weather.weather[2].mintempF)}°F`
+            dayAfterMin.innerHTML = `<span>Lowest:</span> ${weather.weather[2].mintempF}°F`
             dayAfterTomorrow.append(dayAfterHeading,dayAfterAvg,dayAfterMax, dayAfterMin)
 
-
+            
         })
     }
 
@@ -89,11 +103,18 @@ document.querySelector("form").addEventListener("submit", (event) => {
 
 //------------Helper Functions------------
 const capitalize = (location) => {
-    const listWords = location.split(" ")
-    const capLocation = listWords.map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-    return capLocation.join(" ")
+    //Check if location is zip code or other numeric location 
+    if (!isNaN(Number(location))) {
+        return location
+    } else { 
+        const listWords = location.split(" ")
+        const capLocation = listWords.map((word) => {
+            if (word !== "of") {
+               word = word[0].toUpperCase() + word.slice(1).toLowerCase()
+            }
+            return word
+        })
+        return capLocation.join(" ")
+    }
+
 }
-
-// const createWeatherInformation = (weather) => {
-
-// }
